e63a55c5dc93dccd7ac89aa5a39d024069ba43ad
Ivan Liao <ivanhliao@gmail.com>
2022-02-23 17:51:43 -0500

    Update general_prioritized.sql

--- references/sql/general_prioritized.sql
+++ references/sql/general_prioritized.sql
@@ -6,3 +6,11 @@ select City
     ,round(count(*) * 100.0 / sum(count(*)) over (), 2) as percentage
 from Persons
 group by City
+
+-- postgres list stored procedures
+SELECT  nspname, proname 
+FROM    pg_catalog.pg_namespace  
+JOIN    pg_catalog.pg_proc  
+ON      pronamespace = pg_namespace.oid 
+WHERE   nspname = '<schema>'
+ORDER BY Proname 
